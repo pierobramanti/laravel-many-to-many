2a9e72e7102f36aef618a6960148c03ed19b8646
pierobramanti <piero.bramanti94@gmail.com>
2024-10-13 12:40:32 +0200

    [UPD] implemented TechnologySeeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
 
 class TechnologySeeder extends Seeder
 {
@@ -14,6 +15,24 @@ class TechnologySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $techs = [
+            'html',
+            'css',
+            'javascript',
+            'bootstrap',
+            'vue.js',
+            'jquery',
+            'php',
+            'laravel',
+            'reactjs'
+        ];
+
+        foreach ($techs as $tech) {
+            $technology = new Technology(); 
+            $technology->name = $tech;
+            $technology->slug = Technology::generateSlug($tech);  
+
+            $technology->save(); 
+        }
     }
 }
